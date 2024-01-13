8ea0bf82799515a6b06eefd0c8c1d310417e99ca
Zapihra <iida.vaaja@student.lut.fi>
2024-01-13 14:12:59 +0200

    task1 connection with mongodb and registration

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const mongoDB = "mongodb://127.0.0.1:27017/testdb"
+mongoose.connect(mongoDB);
+mongoose.Promise = Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"))
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+  email: {type: String},
+  password: {type: String}
+});
+const User = mongoose.model('Users', userSchema)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/user/register/', function(req, res) {
+  const mail = req.body.email;
+  const pw = req.body.password;
+
+  User.findOne({email: mail}, (err, user) => {
+    if(user) {
+      return res.status(403).json({email: "Email already in use"})
+    }
+    else {
+      var salt = bcrypt.genSaltSync(10)
+      var hash = bcrypt.hashSync(pw,salt)
+      const us = new User({
+        email: mail,
+        password: hash
+      });
+      us.save()
+      res.send("ok")
+    }
+  })
+
+})
 module.exports = router;
