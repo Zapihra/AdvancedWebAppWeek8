99de4601d278447b585ddf24216da2a4e620fb4a
Zapihra <iida.vaaja@student.lut.fi>
2024-01-13 19:58:47 +0200

    task5 adding tasks to list

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,12 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model('Users', userSchema)
 
+const itemSchema = new mongoose.Schema({
+  user: {type: Object},
+  items: {type: Array}
+});
+const Item = mongoose.model('Items', itemSchema)
+
 var opts = {
   secretOrKey: process.env.SECRET,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
@@ -36,7 +42,7 @@ passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//handles the registeration of the user
 router.post('/api/user/register', 
   body("email").trim().isEmail(), 
   body("password").not().isLowercase().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
@@ -49,6 +55,7 @@ function(req, res) {
 
   const mail = req.body.email;
   const pw = req.body.password;
+  console.log(pw)
 
   User.findOne({email: mail}, (err, user) => {
     if(user) {
@@ -66,13 +73,14 @@ function(req, res) {
     }
   })
 })
-
+//handles the users login
 router.post('/api/user/login', function(req, res) {
   User.findOne({email: req.body.email}, (err, user) =>{
     if(!user) {
       return res.status(403).send("No such email")
     }
     else {
+      console.log(req.body.password)
       bcrypt.compare(req.body.password, user.password, (err, match) => {
         if(err) throw err;
         if (match) {
@@ -83,14 +91,39 @@ router.post('/api/user/login', function(req, res) {
             process.env.SECRET
           )
           return res.json({"success":true, "token": token})
+        } else {
+          return res.status(403).send("wrong password")
         }
       })
     }
   })
 })
-
+//only visible if user is authenticated
 router.get('/api/private', passport.authenticate('jwt', {session: false}), (req,res) => {
   res.json({"email": req.user.email})
+})
+//only visible if user is authenticated, either creates new list of adds to the old one
+router.post('/api/todos', passport.authenticate('jwt', {session: false}), (req,res) => {
+  Item.findOne({user: req.user._id}, (err, item) => {
+    if(!item){
+
+      const im = new Item ({
+        user: req.user._id,
+        items: req.body.items
+      })
+      im.save()
+    }
+    else {
+      var list = req.body.items;
+      
+      for (let i = 0; i < list.length; i++) {
+        item.items.push(list[i]) 
+      }
+      item.save();
+      
+    }
+  })
+  res.sendStatus(200)
 })
 
 module.exports = router;
